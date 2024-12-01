f3c4b21e90e689bee75529c051b4fdf02c3bc7b8
Mohame <wtafroach@gmail.com>
2024-12-01 15:17:47 -0500

    styles

--- resources/views/mail/contactNoti.blade.php
+++ resources/views/mail/contactNoti.blade.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Contact Email</title>
-</head>
-<body>
-    <p>Hello {{ $listing->trader->username }},</p>
-    <p>
-        We wanted to let you know that user {{$user->trader->username}} is interested in your listing {{ $listing->title }} you had priced at {{$listing->price}}$. <br>
-        Send them an email: {{$user->email}}.
-    </p>
+<x-layout>
+  <section class="contact-noti">
+        <div class="container">
+            <p>Hello {{ $listing->trader->username }},</p>
+            <p>
+                We wanted to let you know that user {{$user->trader->username}} is interested in your listing {{ $listing->title }} you had priced at {{$listing->price}}$. <br>
+                Send them an email: {{$user->email}}.
+            </p>
 
-    <p>
-        <a href="{{url('/listings/' . $listing->id) }}">View your listing Here</a>
-    </p>
-    <p>Best regards,<br>MARKETHIVE</p>
-</body>
-</html>
+            <p>
+                <a href="{{url('/listings/' . $listing->id) }}">View your listing Here</a>
+            </p>
+            <p>Best regards,<br>MARKETGHOST</p>
+        </div>
+    </section>      
+</x-layout>
